563cbac220fa1e923edbfbf174aa59d62fc5f72b
Olha <Olha.Ishchenko@simcorp.com>
2024-11-04 12:08:03 +0100

    Hello Olha

--- HelloFromMyfirstAPP/Program.cs
+++ HelloFromMyfirstAPP/Program.cs
@@ -10,7 +10,7 @@ namespace HelloFromMyfirstAPP
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello  World");
+            Console.WriteLine("Hello Olha");
         }
     }
 }
